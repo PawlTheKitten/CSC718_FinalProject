36956b3cf0933d11a3da36dfc1effa15625db243
PawlTheKitten <nicholasjstryker@gmail.com>
2020-11-19 14:06:04 -1000

    Uplifting message in oceanNN.

--- oceanNN.py
+++ oceanNN.py
@@ -2,7 +2,4 @@
 
 if __name__ == "__main__": 
     
-    # Print using fstrings
-    name = "NAME"
-    age = "AGE"
-    print (f"Hello, {name}. You are {age}.")
+    print ("This is going to get an A.")
