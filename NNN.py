a0b39b1b6cb9a86c14123e43b8b8ee651e19ea69
PawlTheKitten <nicholasjstryker@gmail.com>
2020-11-19 14:18:05 -1000

    added a neural net test file to work on

---
+++ NNN.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+import matplotlib
+
+if __name__ == "__main__": 
+    
+    print("Version Information:")
+    print (f"Python: {sys.version}")
+    print (f"numpy: {np.__version__}")
+    print (f"Matplotlib: {matplotlib.__version__}")
+
+
+    
